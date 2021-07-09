416228fe8a0bc3adbdd75bafda829a641db5f740
JimdiGris <jimdigris@mail.ru>
2021-07-09 18:38:21 +0300

    first

---
+++ slider.js
@@ -0,0 +1,30 @@
+'use strict';
+
+(() => {
+    const SLIDER = {
+        BODY: document.querySelector('.slider-carousel').querySelector('ul'),
+        START: 1000,
+        SPEED: 10000
+    };
+
+    if (SLIDER) {
+        let elements = document.querySelectorAll('li');
+        let count = elements.length;
+
+        function animation(number) {
+            elements[number].style.opacity = 0;
+
+            if (number == 0) {
+                number = count; 
+
+                elements.forEach((element) => {
+                    element.style.opacity = 1;
+                });
+            }
+
+            setTimeout(() => { animation(number - 1) }, SLIDER.SPEED);
+        }
+
+        setTimeout(() => { animation(count - 1) }, SLIDER.START);
+    }
+})();
